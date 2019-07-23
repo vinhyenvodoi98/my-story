7667b554710c54ac49cac609f668667763837673
hoang <vinhyenvodoi98@gmail.com>
2019-07-23 12:35:08 +0700

    Setup: eslint and deploy github-pages

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "jsx-quotes": ["error", "prefer-single"],
+    semi: ["error", "always"],
+    quotes: ["error", "double"]
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   }
-}
+};
--- README.md
+++ README.md
@@ -1,34 +1,47 @@
 # ddhoang
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn run build
 ```
 
 ### Run your tests
+
 ```
 yarn run test
 ```
 
 ### Lints and fixes files
+
 ```
 yarn run lint
 ```
 
 ### Run your unit tests
+
 ```
 yarn run test:unit
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### deploy to github page
+
+- comment /dist trong file .gitignore
+- run `git add dist && git commit -m "Initial dist subtree commit"`
+- run `git subtree push --prefix dist origin gh-pages`
