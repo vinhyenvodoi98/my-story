67549ca04483c4882f14a062b646e3fb5aea16cc
hoang <vinhyenvodoi98@gmail.com>
2019-07-24 16:00:52 +0700

    Refactor structure

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import store from "./store/index";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./assets/sass/index.sass";
+import "animate.css";
+import "fullpage-vue/src/fullpage.css";
+import VueFullpage from "fullpage-vue";
+
+Vue.use(VueFullpage);
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,108 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="fullpage-container">
+    <div class="fullpage-wp" v-fullpage="opts" ref="example">
+      <div class="page-1 page">
+        <h1 class="part-1" v-animate="{value: 'bounceInRight'}">Hello</h1>
+        <p class="part-1" v-animate="{value: 'bounceInLeft'}">fullpage-vue</p>
+      </div>
+      <div class="page-2 page">
+        <p class="part-2" v-animate="{value: 'bounceInRight'}">fullpage-vue</p>
+      </div>
+      <div class="page-3 page">
+        <p class="part-3" v-animate="{value: 'bounceInLeft', delay: 0}">fullpage-vue</p>
+        <p class="part-3" v-animate="{value: 'bounceInRight', delay: 600}">fullpage-vue</p>
+        <p class="part-3" v-animate="{value: 'zoomInDown', delay: 1200}">fullpage-vue</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+.page-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  margin: 0;
+}
+
+.fullpage-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.page {
+  display: block;
+  text-align: center;
+  font-size: 26px;
+  color: #eee;
+}
+
+.page-1 {
+  padding-top: 100px;
+  background: #1bbc9b;
+}
+
+.page-2 {
+  padding-top: 100px;
+  background-color: rgb(75, 191, 195);
+}
+
+.page-3 {
+  padding-top: 50px;
+  background: #aabbcc;
+}
+
+h3,
+p {
+  font-size: 16px;
+}
+
+.button-group {
+  position: absolute;
+  top: 30px;
+  left: 30px;
+  z-index: 9;
+}
+
+.button-group button {
+  display: inline-block;
+  margin: 10px;
+  color: #000;
+  background: #fff;
+  background: rgba(255, 255, 255, 0.5);
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+  padding: 9px 18px;
+  border: none;
+  outline: none;
+}
+
+.button-group button.active {
+  background: rgba(0, 0, 0, 0.5);
+  color: #fff;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      opts: {
+        start: 0,
+        dir: "v",
+        duration: 500,
+        beforeChange: function(currentSlideEl, currenIndex, nextIndex) {},
+        afterChange: function(currentSlideEl, currenIndex, nextIndex) {}
+      }
+    };
+  }
+};
+</script>
